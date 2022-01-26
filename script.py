c7b2e50fa9ea89699b540764c96d93a547d421ff
Guillaume Falvet <guillaume.falvet@outlook.com>
2022-01-26 18:14:27 +0100

    ignoring gitignore

--- script.py
+++ script.py
@@ -3,21 +3,18 @@ from watchdog.events import FileSystemEventHandler
 import os
 import time
 from datetime import datetime
+import json
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+with open('data.json', 'r') as data_file:
+    data = json.load(data_file)
+directory = []
+extension = []
+for i in data.values():
+    directory.append(i['directory'])
+    extension.append(i['extension'])
+    new_dict = dict(zip(directory, extension))
 
-def log_file(file_extension, file_name, file_location):
-    hour_and_minute = datetime.now().strftime("%H:%M:%S")
-    print(f'{bcolors.UNDERLINE}{hour_and_minute}{bcolors.ENDC} - [{file_extension.upper()}] {bcolors.HEADER}{file_name}{bcolors.ENDC} was moved to {bcolors.OKGREEN}{file_location}{bcolors.ENDC}')
+hour_and_minute = datetime.now().strftime("%H:%M:%S")
 
 
 def file_transfer(file_location, file_name, file_extension, file_source):
@@ -34,44 +31,19 @@ class MyHandler(FileSystemEventHandler):
             split_tup = os.path.splitext(file_name)
             file_extension = split_tup[1]
             # statement for each selected extensions
-            if file_extension == ".jpg" or file_extension == ".jpeg" or file_extension == ".gif" or file_extension == ".png":
-                file_location = folder_img
-                file_transfer(file_location, file_name, file_extension, file_source)
-
-            elif file_extension == ".psd":
-                file_location = folder_psd
-                file_transfer(file_location, file_name, file_extension, file_source)
-
-            elif file_extension == ".pdf":
-                file_location = folder_pdf
-                file_transfer(file_location, file_name, file_extension, file_source)
-
-            elif file_extension == ".zip" or file_extension == ".gz" or file_extension == ".tgz":
-                file_location = folder_zip
-                file_transfer(file_location, file_name, file_extension, file_source)
-
-            elif file_extension == ".ipa":
-                file_location = folder_ipa
-                file_transfer(file_location, file_name, file_extension, file_source)
-
-            elif file_extension == ".dmg":
-                file_location = folder_dmg
-                file_transfer(file_location, file_name, file_extension, file_source)
-
-            else:
-                pass
+            for a, b in new_dict.items():
+                if file_extension == b:
+                    new_destination = a + "/" + file_name
+                    os.rename(file_source, new_destination)
+                    print(f'\033[4m{hour_and_minute}\033[0m - [{file_extension.upper()}] \033[95m{file_name}\033[0m was moved to \033[92m{a}\033[0m')
+                else:
+                    pass
 
 # Directory for each folder
 folder_to_track = "/Users/guillaumefalvet/Downloads"
-folder_img = "/Users/guillaumefalvet/Pictures"
-folder_psd = "/Users/guillaumefalvet/Downlaods/Folder_PSD"
-folder_pdf = "/Users/guillaumefalvet/Downloads/Folder_PDF"
-folder_zip = "/Users/guillaumefalvet/Downloads/Folder_ZIP"
-folder_ipa = "/Users/guillaumefalvet/Downloads/Folder_IPA"
-folder_dmg = "/Users/guillaumefalvet/Downloads/Folder_DMG"
 
 
-print(f'{bcolors.OKCYAN}STARTING{bcolors.ENDC} \n')
+print(f'STARTING \n')
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
